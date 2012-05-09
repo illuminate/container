6599b8d76fea4561a4d1db33c1cd4684a9ef8866
Taylor Otwell <taylorotwell@gmail.com>
2012-05-09 11:31:13 -0500

    added container code.

---
+++ src/Container.php
@@ -0,0 +1,204 @@
+<?php namespace Illuminate\Container;
+
+class BindingResolutionException extends \Exception {}
+
+class Container {
+
+	/**
+	 * The container's bindings.
+	 *
+	 * @var array
+	 */
+	protected $bindings = array();
+
+	/**
+	 * The container's shared instances.
+	 *
+	 * @var array
+	 */
+	protected $instances = array();
+
+	/**
+	 * Create a new container instance.
+	 *
+	 * @param  array  $bindings
+	 */
+	public function __construct($bindings = array())
+	{
+		$this->bindings = array();
+	}
+
+	/**
+	 * Register a binding with the container.
+	 *
+	 * @param  string               $abstract
+	 * @param  Closure|string|null  $concrete
+	 * @param  bool                 $shared
+	 * @return void
+	 */
+	public function bind($abstract, $concrete = null, $shared = false)
+	{
+		if (is_null($concrete)) $abstract = $concrete;
+
+		$this->bindings[$abstract] = compact('concrete', 'shared');
+	}
+
+	/**
+	 * Register a shared binding in the container.
+	 *
+	 * @param  string               $abstract
+	 * @param  Closure|string|null  $concrete
+	 * @return void
+	 */
+	public function shared($abstract, $concrete = null)
+	{
+		return $this->bind($abstract, $concrete, true);
+	}
+
+	/**
+	 * Register an existing instance as shared in the container.
+	 *
+	 * @param  string  $abstract
+	 * @param  mixed   $instance
+	 * @return void
+	 */
+	public function instance($abstract, $instance)
+	{
+		$this->instances[$abstract] = $instance;
+	}
+
+	/**
+	 * Resolve the given type from the container.
+	 *
+	 * @param  string  $abstract
+	 * @return mixed
+	 */
+	public function make($abstract)
+	{
+		// If an instance of the type is currently being managed as a singleton, we will
+		// just return the existing instance instead of instantiating a fresh instance
+		// so the developer can keep re-using the exact same object instance from us.
+		if (isset($this->instances[$abstract]))
+		{
+			return $this->instances[$abstract];
+		}
+
+		// If we don't have a registered resolver or concrete for the type, we'll just
+		// assume the type is the concrete name and will attempt to resolve it as is
+		// since the container should be able to resolve concretes automatically.
+		if ( ! isset($this->bindings[$abstract]))
+		{
+			$concrete = $abstract;
+		}
+		else
+		{
+			$concrete = $this->bindings[$abstract]['concrete'];
+		}
+
+		// We're ready to instantiate an instance of the concrete type registered for
+		// the binding. This will instantiate the type, as well as resolve any of
+		// its nested dependencies recursively until they are each resolved.
+		if ($concrete === $type or $concrete instanceof Closure)
+		{
+			$object = $this->build($concrete);
+		}
+		else
+		{
+			$object = $this->make($concrete);
+		}
+
+		// If the requested type is registered as a singleton, we want to cache off
+		// the instance in memory so we can return it later without creating an
+		// entirely new instances of the object on each subsequent request.
+		if ($this->bindings[$abstract]['shared'])
+		{
+			$this->instances[$abstract] = $object;
+		}
+
+		return $object;
+	}
+
+	/**
+	 * Instantiate an instance of the given type.
+	 *
+	 * @param  string  $type
+	 * @return mixed
+	 */
+	protected function build($type)
+	{
+		// If the concrete type is actually a Closure, we will just execute it and
+		// hand back the results of the function, which allows functions to be
+		// used as resolvers for more fine-tuned resolution of the objects.
+		if ($type instanceof Closure)
+		{
+			return call_user_func_array($type);
+		}
+
+		$reflector = new \ReflectionClass($type);
+
+		// If the type is not instantiable, the developer is attempting to resolve
+		// an abstract type such as an Interface of Abstract Class and there is
+		// no binding registered for the abstraction so we need to bail out.
+		if ( ! $reflector->isInstantiable())
+		{
+			$message = "Target [$type] is not instantiable.";
+
+			throw new BindingResolutionException($message);
+		}
+
+		$constructor = $reflector->getConstructor();
+
+		// If there is no constructor, that means there are no dependencies and
+		// we can just resolve an instance of the object right away without
+		// resolving any other types or dependencies from the container.
+		if (is_null($constructor))
+		{
+			return new $type;
+		}
+
+		$dependencies = $this->getDependencies($constructor->getParameters());
+
+		return $reflector->newInstanceArgs($dependencies);
+	}
+
+	/**
+	 * Resolve all of the dependencies from the ReflectionParameters.
+	 *
+	 * @param  array  $parameterrs
+	 * @return array
+	 */
+	protected function getDependencies($parameters)
+	{
+		$dependencies = array();
+
+		foreach ($parameters as $parameter)
+		{
+			$dependency = $parameter->getClass();
+
+			// If the class is null, it means the dependency is a string or some other
+			// primitive type which we can not esolve since it is not a class and
+			// we'll just bomb out with an error since we have nowhere to go.
+			if (is_null($dependency))
+			{
+				$message = "Unresolvable dependency resolving [$parameter].";
+
+				throw new BindingResolutionException($message);
+			}
+
+			$dependencies[] = $this->resolve($dependency->name);
+		}
+
+		return (array) $dependencies;
+	}
+
+	/**
+	 * Get the container's bindings.
+	 *
+	 * @return array
+	 */
+	public function getBindings()
+	{
+		return $this->bindings;
+	}
+
+}
