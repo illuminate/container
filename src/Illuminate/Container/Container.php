c0784723d145b312f6ca04bbd7084c740e458552
Taylor Otwell <taylorotwell@gmail.com>
2012-05-28 14:28:57 -0500

    throw exception.

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -1,4 +1,4 @@
-<?php namespace Illuminate\Container; use Closure, ArrayAccess;
+<?php namespace Illuminate; use Closure, ArrayAccess;
 
 class BindingResolutionException extends \Exception {}
 
@@ -373,10 +373,12 @@ class Container implements ArrayAccess {
 	 */
 	public function offsetGet($key)
 	{
-		if (array_key_exists($key, $this->bindings))
+		if ( ! array_key_exists($key, $this->bindings))
 		{
-			return $this->make($key);
+			throw new \InvalidArgumentException("Type {$key} is not bound.");
 		}
+
+		return $this->make($key);
 	}
 
 	/**
